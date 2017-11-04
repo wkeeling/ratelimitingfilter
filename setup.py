c9b3750ce4843d309d7a4e4a4a4f2a015f24abf3
Will Keeling <will@zifferent.com>
2017-11-04 09:04:17 +0000

    Add long description

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ try:
 except ImportError:
     from distutils.core import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(name='ratelimitingfilter',
-      version='0.2',
+      version='0.3',
       description='A rate limiting filter for the Python logging system',
+      long_description=long_description,
       url='https://github.com/wkeeling/ratelimitingfilter',
-      download_url='https://github.com/wkeeling/ratelimitingfilter/archive/0.2.tar.gz',
+      download_url='https://github.com/wkeeling/ratelimitingfilter/archive/0.3.tar.gz',
       author='Will Keeling',
       author_email='will@zifferent.com',
       maintainer='Will Keeling',
